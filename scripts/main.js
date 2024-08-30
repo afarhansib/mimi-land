5fad240afd69844066f5a6a99603aaf1f1aa8fe5
Ahmad Farhan Sibghotalloh <afarhansib@gmail.com>
2024-08-30 10:27:35 +0700

    selecting a block test

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,25 @@ import { isMimiItem } from "./utils"
 
 world.afterEvents.itemUse.subscribe(event => {
     if (isMimiItem(event.itemStack)) {
-        event.source.sendMessage("Hi, I'm Mimi!")        
+        // event.source.sendMessage("Hi, I'm Mimi!")        
     }
+})
+
+let lastInteractionTime = 0
+const cooldown = 1000
+
+// apparently playerInteractWithBlock is spamy, so we need to add a cooldown
+world.beforeEvents.playerInteractWithBlock.subscribe(event => {
+    const currentTime = Date.now()
+    if (currentTime - lastInteractionTime < cooldown) {
+        event.cancel = true
+        return
+    }
+    const {block, player, itemStack} = event
+
+    if (isMimiItem(itemStack)) {
+        event.player.sendMessage("Selecting a block...")
+    }
+
+    lastInteractionTime = currentTime
 })
